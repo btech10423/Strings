dd53770ed8c078d43a15dbef296f40a9fdaa19f9
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-06 14:13:55 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Strings{
+    public static void main(String args[]) {
+        char arr[] = {'a','b','c'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        //Strings are IMMUTABLE;
+    }
+}
