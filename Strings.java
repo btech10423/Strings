f1202deb04adaae990fc695e8e0a51fbcabf3213
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-06 14:23:23 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -3,7 +3,7 @@ public class Strings{
     public static void main(String args[]) {
         char arr[] = {'a','b','c'};
         String str = "abcd";
-        String str2 = new String("xyz");
+        String str2 = new String("xyz@1234");
 
         //Strings are IMMUTABLE;
     }
