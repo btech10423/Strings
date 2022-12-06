a9bb7efb605cc00412b080200458a98b053cd567
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-06 18:13:21 +0530

    Add files via upload

---
+++ Comparestrings.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Comparestrings {
+    public static void main(String args[]) {
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+
+        if (s1 == s2) {
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+        if (s1 == s3) {
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+        if (s1.equals(s3)) {
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
