328b02787dc84cce101986d23e3896097c94f9bf
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-06 18:31:51 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Substring{
+    public static String findsubstring(String str,int si,int ei){
+        String substr="";
+        for(int i=si;i<ei;i++){
+            substr+=str.charAt(i);
+        }
+        return substr;
+    }
+    public static void main(String args[]) {
+        String str = "HelloWorld";
+        System.out.print(str.substring(0,5));
+        //System.out.print(findsubstring(str,0,5));
+    }
+}
