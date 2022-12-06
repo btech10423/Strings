de2e900d0d9acba1b8067064945148cc6d5e2904
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-06 14:28:56 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Stringlength {
+    public static void main(String args[]) {
+        String fullname = "Tony Stark";
+        System.out.println(fullname.length());
+    }
+}
