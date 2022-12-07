d9541b7775b99717b5a599923169402d3be2a44c
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-07 14:46:10 +0530

    Add files via upload

---
+++ Stringbuilders.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Stringbuilders {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("");
+        for(char ch='a';ch<='z';ch++){
+            sb.append(ch);
+        }
+        System.out.print(sb.length());
+    }
+}
